20799e81fa40e67a5d6ae777c890485a7851a0bd
Aleale81 <69593297+Aleale81@users.noreply.github.com>
2021-09-10 19:08:11 +0200

    installed axios, created function to show the real-time weather instead of fake data, using api call

--- src/WeatherCard.js
+++ src/WeatherCard.js
@@ -1,24 +1,46 @@
 /** @format */
 
-import React from "react";
+import React, { useState } from "react";
 import Today from "./Today";
 import Search from "./Search";
 import City from "./City";
 import WeatherInfo from "./WeatherInfo";
 import Forecast from "./Forecast";
+import axios from "axios";
 import "./WeatherCard.css";
 
 export default function WeatherCard() {
+	const [city, setCity] = useState("Amsterdam");
+	const [weather, setWeather] = useState({});
+
+	function showWeather(response) {
+		setCity({
+			name: response.data.name,
+			country: response.data.sys.country,
+			temp: Math.round(response.data.main.temp),
+		});
+		setWeather({
+			sky: response.data.weather[0].description,
+			humidity: response.data.main.humidity,
+			wind: Math.round(response.data.wind.speed * 3.6),
+			icon: response.data.weather[0].icon,
+		});
+	}
+
+	let apiKey = `a1bed3af7d71b323ab4af9036fcd943d`;
+	let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+	axios.get(apiUrl).then(showWeather);
 	return (
 		<div className="WeatherCard afternoon">
 			<Today day="Saturday" date={14} month="August" hour={15} minutes={47} />
 			<Search />
-			<City name="amsterdam" country="NL" temperature="24" />
+			<City name={city.name} country={city.country} temperature={city.temp} />
 			<WeatherInfo
-				sky="few clouds"
-				humidity={64}
-				wind={10}
-				icon="http://openweathermap.org/img/wn/02d@2x.png"
+				sky={weather.sky}
+				humidity={weather.humidity}
+				wind={weather.wind}
+				icon={`http://openweathermap.org/img/wn/${weather.icon}@2x.png`}
 			/>
 			<Forecast />
 		</div>
